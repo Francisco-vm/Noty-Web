71984a1292df4defbf8a015900e515f235bb6eb1
Francisco-vm <j9f7v3m@tuta.io>
2025-09-19 17:29:13 -0700

    Add. - Database connector file

--- App/Config/database.php
+++ App/Config/database.php
@@ -1,15 +1,43 @@
 <?php
-
 namespace App\Config;
 
 use PDO;
 use PDOException;
+use Dotenv\Dotenv;
 
-class Database{
-    private static ?self $instance = null;
-    private PDO $pdo;
-    private string $appEnv;
+class Database
+{
+    private static ?PDO $connection = null;
 
-    private function __construct(){
+    public static function connect(): PDO
+    {
+        if (self::$connection === null) {
+            // Variables .env
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+            $dotenv->load();
+
+            $host = $_ENV['DB_HOST'];
+            $port = $_ENV['DB_PORT'];
+            $dbname = $_ENV['DB_NAME'];
+            $charset = $_ENV['DB_CHARSET'] ?? 'utf8mb4';
+            $timezone = $_ENV['DB_TIMEZONE'] ?? 'UTC';
+
+            $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
+
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET time_zone = '{$timezone}'"
+            ];
+
+            try {
+                self::$connection = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASS'], $options);
+            } catch (PDOException $e) {
+                throw new PDOException("Error de conexión: " . $e->getMessage());
+            }
+        }
+
+        return self::$connection;
     }
 }
